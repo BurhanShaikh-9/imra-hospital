cb59d7c5f0288571cebcbbe78c8758e672fd40f5
Burhan Ali Shaikh <85751219+Nyx-95@users.noreply.github.com>
2023-12-22 16:00:16 +0500

    update

--- src/Website/pages/UserProfile.jsx/profile.jsx
+++ src/Website/pages/UserProfile.jsx/profile.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { isValidElement, useEffect, useState } from 'react'
 import profilePic from '../../../assets/images/guy.png'
 import { AdminService } from '../../../services/admin'
 import TokenService from '../../../services/tokenService';
 import { toast } from 'react-toastify';
 import Loader from '../../components/loader';
+import { phoneValidation } from '../../../services/regex';
 
 export const Profile = () => {
 
     const { getSingleAdmin, patchAdmin } = AdminService();
     const { getUserCookie } = TokenService();
     const [isLoading, setIsLoading] = useState(false);
+    const [isValidPhone, setIsValidPhone] = useState(true);
 
     let userId = getUserCookie()
     const [userObject, setUserObject] = useState({});
@@ -29,10 +31,29 @@ export const Profile = () => {
 
 
 
+    // const getInput = (e) => {
+    //     const fieldValue = e.target.value;
+    //     const fieldName = e.target.name;
+    //     setUserObject({ ...userObject, [fieldName]: fieldValue });
+    // };
+
+    const validatePhone = (phone) => {
+        return phoneValidation.test(phone);
+    };
+
     const getInput = (e) => {
         const fieldValue = e.target.value;
         const fieldName = e.target.name;
-        setUserObject({ ...userObject, [fieldName]: fieldValue });
+        if (fieldName === 'phonenumber') {
+            const isValid = validatePhone(fieldValue);
+            // console.log(fieldValue,isValidPhone,'vallll');
+            setIsValidPhone(isValid);
+            if (isValid) {
+                setUserObject({ ...userObject, [fieldName]: fieldValue });
+            }
+        } else {
+            setUserObject({ ...userObject, [fieldName]: fieldValue });
+        }
     };
 
     const getImageInput = (e) => {
@@ -125,13 +146,24 @@ export const Profile = () => {
                                                         />
                                                     </div>
                                                 </div>
-                                                <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
+                                                {/* <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
                                                     <div className="fields">
                                                         <label htmlFor="doctorName">Phone</label>
                                                         <input type="number" onChange={getInput} id="doctorName" name="phonenumber" placeholder={userObject.phonenumber}
                                                         />
                                                     </div>
+                                                </div> */}
+
+                                                <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
+                                                    <div className="fields fieldErrorRelative">
+                                                        <label htmlFor="doctorName">Phone</label>
+                                                        <input className={!isValidPhone && 'errorValidation'} type="number" id="doctorName" name="phonenumber" placeholder={userObject.phonenumber}
+                                                            onChange={getInput}
+                                                        />
+                                                        {!isValidPhone && <p className='erroValidationText'>Invalid Phone Number</p>}
+                                                    </div>
                                                 </div>
+
                                                 <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
                                                     <div className="fields">
                                                         <label htmlFor="doctorName">Branch</label>
@@ -150,7 +182,7 @@ export const Profile = () => {
 
                                                 <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 ">
                                                     <div className="fields">
-                                                        <button type="Submit" >
+                                                        <button type="Submit" disabled={!isValidPhone} >
                                                             Submit
                                                         </button>
                                                     </div>
